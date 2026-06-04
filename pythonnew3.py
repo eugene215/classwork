2c1265b7bb57d5bcd0dab3f53a0853ec6b289960
Взрослая академия <georgygevorkov@gmail.com>
2026-06-04 21:14:49 +0300

    if

---
+++ pythonnew3.py
@@ -0,0 +1,57 @@
+##
+num1=int(input())
+num2=int(input())
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
+
+
+d1= float(input())
+d2= float(input())
+d3=d1*d2
+print(d3)
+
+
+
+numb1=int(input("введите число:"))
+n1=numb1%10
+n2=numb1%100//10
+n3=numb1//10
+print((n1*100)+(n2*10)+ n3)
+##
+
+num1= int(input())
+num2=int(input())
+num3=int(input())
+if num1>num2>num3:
+    print(num1)
+    if num2>num1>num3:
+        print(num2)
+
+
+
+#3
+num1=int(input())
+if num1>=0 and num1<=10
+    print(num1)
+else:
+    print(false)
+
+
+num1=int(input())
+    if
+
+
+num1=int(input())
+if num1<=10:
+    print(num1)
+    if num1>=0:
+print(num1)
+
+
+
+
+
+
+

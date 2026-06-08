70d76edfd7f4a384e55a58116b3137ecb6d2c6b4
Взрослая академия <georgygevorkov@gmail.com>
2026-06-08 21:14:51 +0300

    cycles

---
+++ 08.py
@@ -0,0 +1,101 @@
+'''
+num1=int(input())
+num2=int(input())
+num3=int(input())
+if num1>num2 and num1>num3;
+    print(num1)
+elif num2>num3;
+    print(num2)
+else;
+print (num3)
+
+
+
+t=int(input())
+print(a%2=0)
+
+
+
+
+    d=int(input())
+    print(10<=d<=20)
+
+
+
+    a=int(input())
+    if a//10=3 or a%10=3;
+        print(a)
+
+
+
+a=int(input())
+b=int(input())
+print(a%3=0 and b%3=0)
+'''
+'''
+n=int(input())
+while n>=0:
+    print(n)
+    n=n-1
+
+n=int(input())
+a=0
+while a<=n:
+    print(a)
+    a=a+1
+'''
+'''
+a=int(input())
+b=int(input())
+if a>b:
+    a,b=b,a
+while a<=b:
+    print(a)
+    a=a+1
+'''
+'''
+n=0
+while n<=10:
+    print(n)
+    n+=2
+'''
+
+a=int(input())
+b=int(input())
+if a>b: a,b=b,a
+if a%2: a+=1
+while a<=b:
+    print(a)
+    a+=2
+
+
+a=int(input())
+n=3
+if
+while n<=a:
+    print(n)
+    n+=10
+
+
+a=int(input("сколько чисел:"))
+s=0
+while a>=0:
+    num=int(input())
+    s+=num
+    a+=1
+    print (s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

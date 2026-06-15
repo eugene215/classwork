3bf4f80fd6e8bf194963f0eba3dc80970663df46
Взрослая академия <georgygevorkov@gmail.com>
2026-06-15 21:11:10 +0300

    cycles

---
+++ 15-06.py
@@ -0,0 +1,107 @@
+'''
+a=int(input())
+b=int(input())
+summa=0
+for i in range(a,b+1):
+
+    print(i)
+
+
+fact=1
+n=int(input())
+for i in range(2,n+1):
+    fact*=i
+    print(fact)
+
+
+
+a=int(input())
+for i in range(4):
+    if max<a:
+'''
+
+#
+# column=int(input())
+# raw=int(input())
+# for i in range(raw):
+#    print("*"*column)
+#
+
+'''
+n=int(input())
+for i in range(n):
+    print("*"*n)
+'''
+'''4
+
+column=int(input())
+raw=int(input())
+for i in range(raw):
+    for j in range(column):
+        print("*",end=" ")
+    print()
+'''
+
+'''
+column=int(input())
+raw=int(input())
+k=1
+for i in range(raw):
+    for j in range(column):
+        print(k,end=" ")
+        k+=1
+    print()
+ '''
+
+
+'''
+column=int(input())
+raw=int(input())
+for i in range(1,raw,raw+1):
+    for j in range(1,column,column+1):
+        print(f"{i}:{j}")
+    print()
+'''
+
+'''
+n=int(input())
+for i in range(n):
+    print("*"*(n-i))
+'''
+'''
+n=int(input())
+for i in range(1,n):
+    print("  "*(n-i)+"* "*(i))
+
+
+n=int(input())
+for i in range(n):
+    print("* "*(i)+"  "*(n-i))
+
+
+n=int(input())
+print("* "*n)
+for i in range(n-2):
+    print("* "+ "  "*(n-2)+"*")
+print("* "*n) 
+'''
+
+n=int(input())
+for i in range((n+1)//2):
+    print("  "*i+"* "*(n-i*2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
